0d8c942888bac4900d0dc088a06d9605f7e31dc0
Aaron Kunert <aaron.kunert@koona-labs.de>
2026-05-04 17:16:13 +0200

    Scaffold tableau

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
+<template>
+    <div class="p-10 bg-gray-100">
+        
+        <div class="bg-green-400 border border-gray-800 rounded-xl p-6 grid grid-cols-3">
+            <div></div>
+            <div class="grid grid-cols-[min-content_min-content_min-content]">
+                <div class="grid col-span-3 grid-cols-subgrid border border-yellow-300">
+                    <div class="col-start-2">0</div>
+                </div>
+                <div class="border border-yellow-300 size-12 flex items-center justify-center">1</div>
+                <div class="border border-yellow-300 size-12 flex items-center justify-center">2</div>
+                <div class="border border-yellow-300 size-12 flex items-center justify-center">3</div>
+                <div class="border border-yellow-300 size-12 flex items-center justify-center">4</div>
+                <div class="border border-yellow-300 size-12 flex items-center justify-center">5</div>
+                <div class="border border-yellow-300 size-12 flex items-center justify-center">6</div>
+
+
+
+            </div>
+            <div></div>
+            
+            
+        </div>
+        
+    </div>
+</template>
+
+
 <script setup>
 
-import HelloWorld from './components/HelloWorld.vue'
 </script>
+<style>
 
-<template>
-    
-    <div class="h-48 bg-red-200">Just a test</div>
-    <HelloWorld/>
-</template>
+</style>
