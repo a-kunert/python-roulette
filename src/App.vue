bd223ada8112538ea1915d41e4f306fc080f2a96
yannis <yannis.nill@icloud.com>
2026-05-04 17:32:41 +0200

    Numbers completed

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,44 @@
             <div></div>
             <div class="grid grid-cols-[min-content_min-content_min-content]">
                 <div class="grid col-span-3 grid-cols-subgrid border border-yellow-300">
-                    <div class="col-start-2">0</div>
+                    <div class="col-start-2 flex items-center justify-center text-amber-300">0</div>
                 </div>
-                <div class="border border-yellow-300 size-12 flex items-center justify-center">1</div>
-                <div class="border border-yellow-300 size-12 flex items-center justify-center">2</div>
-                <div class="border border-yellow-300 size-12 flex items-center justify-center">3</div>
-                <div class="border border-yellow-300 size-12 flex items-center justify-center">4</div>
-                <div class="border border-yellow-300 size-12 flex items-center justify-center">5</div>
-                <div class="border border-yellow-300 size-12 flex items-center justify-center">6</div>
-
-
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">1</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">2</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">3</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">4</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">5</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">6</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">7</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">8</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">9</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">10</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">11</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">12</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">13</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">14</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">15</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">16</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">17</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">18</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">19</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">20</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">21</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">22</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">23</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">24</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">25</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">26</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">27</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">28</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">29</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">30</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">31</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">32</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">33</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">34</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center text-red-600">35</div>
+              <div class="border border-yellow-300 size-12 flex items-center justify-center">36</div>
 
             </div>
             <div></div>
