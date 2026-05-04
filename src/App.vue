c77357ff56aa04ce007531c8602b3cd1333bd815
Aaron Kunert <aaron.kunert@koona-labs.de>
2026-05-04 16:25:33 +0200

    Pull in VueJs

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    
+    <div class="h-48 bg-red-200">Just a test</div>
+    <HelloWorld/>
+</template>
