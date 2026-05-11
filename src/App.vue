b3609186ad6eadd9eb7caeee98d9befe906d8d7a
Aaron Kunert <aaron.kunert@koona-labs.de>
2026-05-11 14:29:52 +0200

    Clean up collapsing borders

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,63 @@
 <template>
     <div class="p-10 bg-gray-100">
-        
+
         <div class="bg-[#049B04] border border-gray-800 rounded-xl p-6 grid grid-cols-3">
 
-          <div></div>
+            <div></div>
 
             <div class="flex flex-col items-center">
-              <div class="border border-yellow-400 w-48 h-10 flex items-center justify-center text-yellow-300 font-extrabold font-serif text-2xl">
-                0
-              </div>
+                <div
+                    class="border-x border-t border-yellow-400 w-48 h-10 flex items-center justify-center text-yellow-300 font-extrabold font-serif text-2xl">
+                    0
+                </div>
 
-            <div class="grid grid-cols-3">
+                <div class="grid grid-cols-3">
 
-              <div
-                  class="border border-yellow-400 w-16 h-10 flex items-center justify-center font-serif text-2xl"
-                  :class="[getColor(number), number.color !== 'green' ? 'font-extrabold': '']"
-                  v-for="number in numbers.filter(number => number.number !== 0)"
-                  :key="number.number"
-                >
-                {{ number.number }}
-              </div>
+                    <div
+                        class="border-l border-t border-yellow-400 w-16 h-10 flex items-center justify-center font-serif text-2xl font-extrabold"
+                        :class="[
+                            getColor(number), 
+                        number.number % 3 === 0 ? 'border-r' : ''
+                  ]"
+                        v-for="number in numbers.filter(number => number.number !== 0)"
+                        :key="number.number"
+                    >
+                        {{ number.number }}
+                    </div>
 
-            </div>
+                </div>
 
-              <div class="grid grid-cols-3">
-                <div class="border border-yellow-400 w-16 h-10"></div>
-                <div class="border border-yellow-400 w-16 h-10"></div>
-                <div class="border border-yellow-400 w-16 h-10"></div>
-              </div>
-
-            </div>
-
-          <div class="flex flex-col mt-10">
-
-            <div class="border border-yellow-400 w-32 h-[240px] flex items-center justify-center text-yellow-300 font-extrabold font-serif text-3xl">
-
-              <div class="rotate-180 [writing-mode:vertical-rl]">
-                MANQUE
-              </div>
+                <div class="grid grid-cols-3">
+                    <div class="border-y border-l border-yellow-400 w-16 h-10"></div>
+                    <div class="border-y border-l border-yellow-400 w-16 h-10"></div>
+                    <div class="border border-yellow-400 w-16 h-10"></div>
+                </div>
 
             </div>
 
-            <div class="border border-yellow-400 w-32 h-[240px] flex items-center justify-center text-yellow-300 font-extrabold font-serif text-3xl">
+            <div class="flex flex-col mt-10">
 
-              <div class="rotate-180 [writing-mode:vertical-rl]">
-                IMPAIR
-              </div>
+                <div
+                    class="border border-yellow-400 w-32 h-[240px] flex items-center justify-center text-yellow-300 font-extrabold font-serif text-3xl">
+
+                    <div class="rotate-180 [writing-mode:vertical-rl]">
+                        MANQUE
+                    </div>
+                </div>
+
+                <div
+                    class="border border-yellow-400 w-32 h-[240px] flex items-center justify-center text-yellow-300 font-extrabold font-serif text-3xl">
+
+                    <div class="rotate-180 [writing-mode:vertical-rl]">
+                        IMPAIR
+                    </div>
+
+                </div>
 
             </div>
-
-          </div>
 
         </div>
-        
+
     </div>
 </template>
 
@@ -60,45 +65,44 @@
 <script setup>
 
 
-
 const numbers = [
-    {"number": 0, "color": "green"},
-    {"number": 1, "color": "red"},
-    {"number": 2, "color": "black"},
-    {"number": 3, "color": "red"},
-    {"number": 4, "color": "black"},
-    {"number": 5, "color": "red"},
-    {"number": 6, "color": "black"},
-    {"number": 7, "color": "red"},
-    {"number": 8, "color": "black"},
-    {"number": 9, "color": "red"},
-    {"number": 10, "color": "black"},
-    {"number": 11, "color": "black"},
-    {"number": 12, "color": "red"},
-    {"number": 13, "color": "black"},
-    {"number": 14, "color": "red"},
-    {"number": 15, "color": "black"},
-    {"number": 16, "color": "red"},
-    {"number": 17, "color": "black"},
-    {"number": 18, "color": "red"},
-    {"number": 19, "color": "red"},
-    {"number": 20, "color": "black"},
-    {"number": 21, "color": "red"},
-    {"number": 22, "color": "black"},
-    {"number": 23, "color": "red"},
-    {"number": 24, "color": "black"},
-    {"number": 25, "color": "red"},
-    {"number": 26, "color": "black"},
-    {"number": 27, "color": "red"},
-    {"number": 28, "color": "black"},
-    {"number": 29, "color": "black"},
-    {"number": 30, "color": "red"},
-    {"number": 31, "color": "black"},
-    {"number": 32, "color": "red"},
-    {"number": 33, "color": "black"},
-    {"number": 34, "color": "red"},
-    {"number": 35, "color": "black"},
-    {"number": 36, "color": "red"}
+    { "number": 0, "color": "green" },
+    { "number": 1, "color": "red" },
+    { "number": 2, "color": "black" },
+    { "number": 3, "color": "red" },
+    { "number": 4, "color": "black" },
+    { "number": 5, "color": "red" },
+    { "number": 6, "color": "black" },
+    { "number": 7, "color": "red" },
+    { "number": 8, "color": "black" },
+    { "number": 9, "color": "red" },
+    { "number": 10, "color": "black" },
+    { "number": 11, "color": "black" },
+    { "number": 12, "color": "red" },
+    { "number": 13, "color": "black" },
+    { "number": 14, "color": "red" },
+    { "number": 15, "color": "black" },
+    { "number": 16, "color": "red" },
+    { "number": 17, "color": "black" },
+    { "number": 18, "color": "red" },
+    { "number": 19, "color": "red" },
+    { "number": 20, "color": "black" },
+    { "number": 21, "color": "red" },
+    { "number": 22, "color": "black" },
+    { "number": 23, "color": "red" },
+    { "number": 24, "color": "black" },
+    { "number": 25, "color": "red" },
+    { "number": 26, "color": "black" },
+    { "number": 27, "color": "red" },
+    { "number": 28, "color": "black" },
+    { "number": 29, "color": "black" },
+    { "number": 30, "color": "red" },
+    { "number": 31, "color": "black" },
+    { "number": 32, "color": "red" },
+    { "number": 33, "color": "black" },
+    { "number": 34, "color": "red" },
+    { "number": 35, "color": "black" },
+    { "number": 36, "color": "red" }
 ]
 
 
